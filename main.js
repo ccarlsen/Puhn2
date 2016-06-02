072dcf8c7080fd507d50208e26d4f63a8c2b7eaf
Martin <madetho@live.de>
2016-06-02 18:42:53 +0200

    Copy&Paste on OSX

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 const electron			= require('electron');
-const app				= electron.app;
-const BrowserWindow		= electron.BrowserWindow;
-const ipcMain			= electron.ipcMain;
+const {Menu, MenuItem, app, BrowserWindow, ipcMain} = electron;
 
 let loginWindow;
 let mainWindow;
@@ -27,6 +25,27 @@ app.on('ready', function() {
 		mainWindow.loadURL(`file://${__dirname}/index.html`);
 		loginWindow.close();
 		//mainWindow.webContents.openDevTools();
+
+		var template = [{
+        label: "Application",
+        submenu: [
+            { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+            { type: "separator" },
+            { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+        ]}, {
+        label: "Edit",
+        submenu: [
+            { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+            { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+            { type: "separator" },
+            { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+            { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+            { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+            { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+        ]}
+    ];
+
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 
 	});
 });
